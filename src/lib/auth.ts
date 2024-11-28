80cc7b09eaf0283b4804b0ab04f03a7593b50387
nihalnaveed12 <nihalnaveed50@gmail.com>
2024-11-28 20:22:43 +0500

    update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
-import { NextAuthOptions } from "next-auth";
+
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
   providers: [
