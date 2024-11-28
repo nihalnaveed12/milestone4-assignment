f55e82013f810309f5d5d3c5b83f1d3459905e3c
nihalnaveed12 <nihalnaveed50@gmail.com>
2024-11-28 20:08:24 +0500

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,10 +3,7 @@ import { signIn } from "next-auth/react";
 
 export default function Login() {
   
-  const handleLogin = async () => {
-    
-    signIn("google");
-  };
+  
 
   return (
     <div
@@ -16,8 +13,8 @@ export default function Login() {
       <h1 className="text-2xl font-semibold">Login</h1>
       <div className="flex flex-col w-[20%] gap-4 items-center ">
         <button
-          type="submit"
-          onClick={handleLogin}
+          type="button"
+          onClick={() => signIn()}
           className="bg-zinc-50 px-2 py-1 w-24 text-zinc-900 hover:bg-zinc-200 rounded-lg"
         >
           Login
